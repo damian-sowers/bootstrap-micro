2ad8a5f279ff2c4524cc248c5d182db9f107c2d7
Damian Sowers <damian.sowers@gmail.com>
2013-02-20 16:04:10 -0800

    error

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module RetrieverHerokuCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #to satisfy heroku
+    config.assets.initialize_on_precompile = false
   end
 end
