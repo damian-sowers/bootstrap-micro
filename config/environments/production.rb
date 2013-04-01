0d53c284dfb7b84971e18f4a1964a4721075ce80
Damian Sowers <damian.sowers@gmail.com>
2013-04-01 14:19:19 -0700

    add email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,21 @@ RetrieverHerokuCom::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://www.bootstrapmicro.com' }
+  config.action_mailer.default_url_options = { :host => 'https://bootstrapmicro.herokuapp.com/' }
+
+    config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "bootstrapmicro.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true, 
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   config.assets.initialize_on_precompile = false
 end
